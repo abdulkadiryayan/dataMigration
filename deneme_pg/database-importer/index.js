0384ef70463e75ef98536e60e2cb39f9a7a38bb9
abdulkadiryayan <abdulkadiryayan@hotmail.com>
2024-05-13 13:53:01 +0300

    istekler req body içerisinde gidecek şekilde yapıldı.

--- deneme_pg/database-importer/index.js
+++ deneme_pg/database-importer/index.js
@@ -2,45 +2,61 @@ const express = require('express');
 const { exec } = require('child_process');
 const fs = require('fs');
 require('dotenv').config();
+const path = require('path'); // path modülünü ekleyin
 
-// index2.js'den fonksiyonları import etme
-const { applySqlScript } = require('./index2');
+
+const { applySqlScript } = require('./index2'); // index2.js'den fonksiyonları import etme
 
 const app = express();
+
+app.use(express.json()); // JSON verilerini işlemek için express.json() ara yazılımını ekleyin
 
 // Port
 const PORT = process.env.PORT || 3000;
 
 // Dosya yolları
-const dumpFile = './backups/dump.sql';
+const backupsDirectory = './backups/';
 
 const sqlScriptPathMigrate = 'C:/Users/abdulkadir/Desktop/dosya/migration_v1_to_v2.sql';
 const sqlScriptPathRollback = 'C:/Users/abdulkadir/Desktop/dosya/rollback_v2_to_v1.sql';
 
-// Veritabanı bağlantı bilgileri
-const exportFrom = {
-    host: "localhost",
-    user: "postgres",
-    password: process.env.pass,
-    database: "dvdrental"
-};
+// Backup dosyalarını listeleme endpoint'i
+app.get('/backup_list', (req, res) => {
+    fs.readdir(backupsDirectory, (err, files) => {
+        if (err) {
+            console.error('Error reading backup directory:', err);
+            res.status(500).send('Error reading backup directory');
+            return;
+        }
 
-const exportCon = `postgresql://${exportFrom.user}:${exportFrom.password}@${exportFrom.host}:5432/${exportFrom.database}`;
+        // Sadece .sql uzantılı dosyaları filtreleme
+        const backupFiles = files.filter(file => path.extname(file) === '.sql');
 
-const importTo = {
-    host: "localhost",
-    user: "postgres",
-    password: process.env.pass,
-    database: "dvdrental1"
-}; 
-
-const importCon = `postgresql://${importTo.user}:${importTo.password}@${importTo.host}:5432/${importTo.database}`;
+        // Dosya listesini istemciye gönderme
+        res.status(200).json(backupFiles);
+    });
+});
 
 // Dump endpoint'i
 app.post('/dump', (req, res) => {
-    console.log(`Veriler ${exportFrom.database} veritabanından dışa aktarılmaya başlanıyor`);
+    const { target_db_connection } = req.body;
+    const { host, user, password, database } = target_db_connection;
 
-    exec(`pg_dump -Fc ${exportCon} > ${dumpFile}`, { maxBuffer: 1024 * 1024 * 10 }, (err, stdout, stderr) => {
+    const exportFrom = {
+        host: host,
+        user: user,
+        password: password,
+        database: database
+    };
+
+    const exportCon = `postgresql://${exportFrom.user}:${process.env.PGPASSWORD}@${exportFrom.host}:5432/${exportFrom.database}`;
+
+    const uniqueFileName = `backup_${new Date().toISOString().replace(/:/g, '_').replace(/\..+/, '')}.sql`;
+    const dumpFilePath = backupsDirectory + uniqueFileName;
+
+    console.log(`Veriler ${exportFrom.database} veritabanından dışa aktarılarak ${uniqueFileName} dosyasına kaydediliyor`);
+
+    exec(`pg_dump -Fc ${exportCon} > ${dumpFilePath}`, { maxBuffer: 1024 * 1024 * 10 }, (err, stdout, stderr) => {
         if (err) {
             console.error(`exec error: ${err}`);
             res.status(500).send('Veri dışa aktarma işlemi sırasında bir hata oluştu. ' + err);
@@ -48,16 +64,28 @@ app.post('/dump', (req, res) => {
         }
 
         console.log("Dışa aktarma işlemi tamamlandı.")
-        res.status(200).send(`Dışa aktarma işlemi tamamlandı.`);
-        //res.status(200).sendFile(dumpFile);
+        res.status(200).send(`Dışa aktarma işlemi tamamlandı ve ${uniqueFileName} dosyasına kaydedildi.`);
     });
 });
 
 // Restore endpoint'i
 app.post('/restore', (req, res) => {
+    const { source_backup_file, target_db_connection } = req.body;
+    const { host, user, password, database } = target_db_connection;
+
+    const importTo = {
+        host: host,
+        user: user,
+        password: password,
+        database: database
+    };
+
+    const importCon = `postgresql://${importTo.user}:${process.env.PGPASSWORD}@${importTo.host}:5432/${importTo.database}`;
+    const backupFilePath = backupsDirectory + source_backup_file;
+
     console.log(`Veriler ${importTo.database} veritabanına aktarılıyor`);
 
-    exec(`pg_restore --if-exists=append -c -d ${importCon} ${dumpFile}`, (err, stdout, stderr) => {
+    exec(`pg_restore --if-exists=append -c -d ${importCon} ${backupFilePath}`, (err, stdout, stderr) => {
         if (err) {
             console.error(`exec error: ${err}`);
             res.status(500).send('Veri içe aktarma işlemi sırasında bir hata oluştu. ' + err);
