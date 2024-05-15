f322a12688c980f9df66fb4860857e3687fbb883
Nmexic <mbal7759@gmail.com>
2024-05-16 02:30:58 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import BackupList from './pages/BackupList';
